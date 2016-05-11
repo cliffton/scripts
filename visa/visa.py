d5ab7095686ba8f763ca7727614bec4ea8dea3e6
cliffton <cliffton.fernandes.rulz@gmail.com>
2016-05-12 02:19:33 +0530

    update in date in may

---
+++ visa/visa.py
@@ -0,0 +1,45 @@
+import requests
+from secs import *
+
+
+def send_sms():
+    print "Sending SMS"
+    try:
+        requests.get(GUPSHUP_SMS_URL)
+    except Exception, e:
+        print e
+        pass
+
+
+def send_email():
+    print "Sending send_email"
+
+
+cookies = {
+    "BrowserId": "i_rspoYiQ-KUfFYKZ6z1Sw",
+    "__utma": "1.363385147.1462907492.1462907492.1462996462.2",
+    "__utmb": "1.9.10.1462996462",
+    "__utmc": "1",
+    "__utmt": "1",
+    "__utmz": "1.1462907492.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=https%3A%2F%2Fcgifederal.secure.force.com%2F",
+    "apex__aa-time": "KeNHnxhHDNBY1uLE_2F6f4wg_3D_3D",
+    "autocomplete": "1",
+    "clientSrc": "27.4.199.208",
+    "inst": "APP_1A",
+    "lloopch_loid": "00DC0000000PhuP",
+    "lloopch_lpid": "060C0000000QwL9",
+    "oid": "00DC0000000PhuP",
+    "oinfo": "c3RhdHVzPUFDVElWRSZ0eXBlPTYmb2lkPTAwREMwMDAwMDAwUGh1UA==",
+    "sid": "00DC0000000PhuP!ARwAQHqzr0IS3Cfzi9qTGLJGlxZLJ29oBkXSX.dfT88BV4bzQlOUS7GY91t4WNVXl_28jApcNWeK8OuZu8QyPTnjG3Ob7vez",
+    "sid_Client": "A00000BHCSe0000000PhuP"
+}
+
+resp = requests.get(
+    'https://cgifederal.secure.force.com/applicanthome', cookies=cookies)
+
+index = resp.content.find('First Available Appointment Is ')
+date_str = resp.content[index:index + 53]
+value = date_str.find('May')
+if value > 0:
+    send_email()
+    send_sms()
